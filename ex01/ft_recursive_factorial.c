e592f447c11c2d6e286dece9e6a5f08265d881cb
Amay Moon <amaylemoon@gmail.com>
2023-02-22 17:35:01 +0100

    CJ04

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -2,20 +2,18 @@
 
 int ft_recursive_factorial(int nb)
 {
-	if(nb <  0)
-	return(nb * ft_recursive_factorial(nb + 1));
-		
-	if(nb == 0)
+	if(nb < 0)
 		return(0);
-
-	else if(nb == 1)
+	
+	if(nb == 0 || nb == 1)
 		return(1);
+
 	return(nb * ft_recursive_factorial(nb - 1));
 }
 
 /*
 int main(void)
 {
-	printf("%d\n", ft_recursive_factorial(6));
+	printf("%d\n", ft_recursive_factorial(1));
 }
 */
